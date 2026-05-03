13004ed6bb653268995db2fce50839e4751e047d
gabrieldemian <gabrielgcr45@gmail.com>
2026-05-03 19:55:57 +0200

    first commit

---
+++ .config/nvim/init.lua
@@ -0,0 +1,269 @@
+local function branch()
+	local ohgod = vim.fn.system("git branch --show-current")
+	local branch = string.gsub(ohgod, "%s+$", "")
+	if not branch or branch == "" or branch:find("^fatal: not a git repo") then
+		return ""
+	end
+	return "     " .. branch
+end
+
+local function smart_enter()
+	local line        = vim.fn.getline('.')
+	local col         = vim.fn.col('.')
+	local left        = line:sub(1, col - 1):match('^(.*%S)%s*$')
+	local right, rest = line:sub(col):match('^%s*(%S)(.*)')
+	local pairs       = { ['{'] = '}', ['('] = ')', ['['] = ']' }
+	if left and right and pairs[left:sub(-1)] == right then
+		local indent = line:match('^%s*')
+		local tab = vim.bo.expandtab and ' ' or '\t'
+		local inner = indent .. string.rep(tab, vim.bo.shiftwidth)
+		vim.api.nvim_buf_set_lines(0, vim.fn.line('.') - 1, vim.fn.line('.'), false, {
+			indent .. left,        -- opening bracket + preceding text
+			inner,                 -- indented blank line (cursor lands here)
+			indent .. right .. (rest or '') -- closing bracket + everything after
+		})
+		vim.api.nvim_win_set_cursor(0, { vim.fn.line('.') + 1, #inner + 1 })
+		return
+	end
+	-- normal enter
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<CR>', true, true, true), 'n', true)
+end
+
+-------------------
+----- options -----
+-------------------
+
+vim.g.mapleader = " "
+vim.cmd("colorscheme default")
+vim.g.autoformat = true
+vim.g.markdown_recommended_style = 0
+vim.wo.number = true
+vim.wo.numberwidth = 2
+vim.wo.scrolloff = 8
+vim.wo.sidescrolloff = 4
+vim.wo.list = true
+vim.bo.undofile = true
+vim.bo.shiftwidth = 4
+vim.bo.tabstop = 4
+vim.bo.softtabstop = 4
+-- vim.wo.breakindent = true
+-- vim.treesitter.indent = true
+-- vim.bo.autoindent = true
+local statusline = {
+	' %<%f',
+	branch(),
+	'%m',
+	'%=',
+	'',
+	'%{&filetype}',
+	'  0x%B',
+	' %2p%%',
+	' %3l:%-2c '
+}
+vim.o.statusline = table.concat(statusline, '')
+vim.o.winborder = '╭,-,╮,|,╯,-,╰,|'
+vim.o.autocomplete = true
+vim.o.smartcase = true
+vim.o.updatetime = 250
+vim.o.termguicolors = true
+vim.o.wildmenu = true
+vim.o.wildmode = "longest:full,full"
+vim.o.showcmd = true
+vim.o.showmode = false
+
+-- Disable clipboard in SSH servers
+vim.o.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" -- Sync with system clipboard
+vim.o.textwidth = 80
+vim.opt.completeopt = { "menuone", "noselect", "popup" }
+vim.o.backup = false
+vim.o.writebackup = false
+vim.o.swapfile = false
+vim.o.undofile = true
+vim.o.autoread = true
+vim.o.synmaxcol = 300 -- Syntax highlighting limit
+vim.o.redrawtime = 10000
+vim.o.maxmempattern = 20000
+
+-- make the status bar the same color as the background
+local line_color = vim.api.nvim_get_hl(0, { name = 'Normal' }).bg
+if line_color then
+	vim.api.nvim_set_hl(0, 'StatusLine', { bg = line_color })
+end
+
+vim.filetype.add({
+	extension = {
+		env = "dotenv",
+	},
+	filename = {
+		[".env"] = "dotenv",
+		["env"] = "dotenv",
+	},
+	pattern = {
+		["[jt]sconfig.*.json"] = "jsonc",
+		["%.env%.[%w_.-]+"] = "dotenv",
+	},
+})
+
+-------------------
+----- keymaps -----
+-------------------
+
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+local function with_desc(desc)
+	return { desc = desc, noremap = true, silent = true }
+end
+
+map('i', '<CR>', smart_enter, opts)
+map("n", "WW", ":wa<CR>", opts)
+map("n", "<C-t>", ":tabedit<CR>", with_desc("Create tab"))
+map("n", "<C-b>", ":bd<CR>", with_desc("Delete buffer"))
+map("n", "<Tab>", ":tabnext<CR>", with_desc("Next tab"))
+map("n", "<S-Tab>", ":tabprev<CR>", with_desc("Previous tab"))
+map("n", "<C-Tab>", ":bn<CR>", with_desc("Next buffer"))
+map("n", "<leader>fzf", ":FzfLua<CR>", opts)
+map("n", "ff", ":FzfLua files<CR>", opts)
+map("n", "fg", ":FzfLua grep_visual<CR>", opts)
+map("n", "fk", ":FzfLua keymaps<CR>", opts)
+map("n", "x", "\"_x", opts)
+map({ "n", "v" }, "<leader>y", "\"+y", opts)
+map({ "n", "x" }, "]d", vim.diagnostic.goto_next, with_desc("Next Diagnostic"))
+map({ "n", "x" }, "[d", vim.diagnostic.goto_prev, with_desc("Prev Diagnostic"))
+map("v", "p", '"_dP', opts)
+map({ "i", "c" }, "<C-h>", "<Left>", opts)
+map({ "i", "c" }, "<C-j>", "<Down>", opts)
+map({ "i", "c" }, "<C-k>", "<Up>", opts)
+map({ "i", "c" }, "<C-l>", "<Right>", opts)
+
+-- git signs
+map("n", "<leader>gsh", ':Gitsigns stage_hunk<cr>', opts)
+map("n", "<leader>gSh", ':Gitsigns undo_stage_hunk<cr>', opts)
+map("n", "<leader>gsb", ':Gitsigns stage_buffer<cr>', opts)
+map("n", "<leader>gSb", ':Gitsigns undo_stage_buffer<cr>', opts)
+map("n", "<leader>grb", ':Gitsigns reset_buffer<cr>', opts)
+map("n", "<leader>grh", ':Gitsigns reset_hunk<cr>', opts)
+map("n", "<leader>gph", ':Gitsigns preview_hunk<cr>', opts)
+map("n", "<leader>gd", ':Gitsigns diff_this<cr>', opts)
+
+-- auto close pairs
+map("i", "`", "``<left>")
+map("i", '"', '""<left>')
+map("i", "'", "''<left>")
+map("i", "(", "()<left>")
+map("i", "[", "[]<left>")
+map("i", "{", "{}<left>")
+map("i", "<", "<><left>")
+
+map("n", "-", "<cmd>Oil<cr>", with_desc("Open parent directory"))
+map("n", "<leader>fm", ":lua vim.lsp.buf.format()<CR>", opts)
+-- Fix spelling (picks first suggestion)
+map("n", "z0", "1z=", with_desc("Fix word under cursor"))
+
+-----------------
+---- plugins ----
+-----------------
+
+vim.pack.add({
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/ibhagwan/fzf-lua" },
+	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
+})
+require("mason").setup({})
+require("oil").setup()
+require("fzf-lua").setup()
+require("gitsigns").setup()
+
+-----------------
+------ lsp ------
+-----------------
+
+local function attachme(client, bufnr)
+	vim.lsp.completion.enable(true, client.id, bufnr, {
+		autotrigger = true,
+		convert = function(item)
+			return { abbr = item.label:gsub("%b()", "") }
+		end,
+	})
+	vim.keymap.set("i", "<C-space>", vim.lsp.completion.get, { desc = "trigger autocompletion" })
+end
+
+vim.lsp.config['lua_ls'] = {
+	cmd = { 'lua-language-server' },
+	filetypes = { 'lua' },
+	root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
+	on_attach = attachme,
+	settings = {
+		Lua = {
+			runtime = {
+				version = 'LuaJIT',
+			}
+		}
+	}
+}
+vim.lsp.config['zls'] = {
+	cmd = { 'zls' },
+	filetypes = { 'zig' },
+	on_attach = attachme,
+	settings = {
+		enable_build_on_save = true,
+		warn_style = true
+	}
+}
+vim.lsp.config['rust-analyzer'] = {
+	cmd = { 'rust-analyzer' },
+	filetypes = { 'rust' },
+	on_attach = attachme,
+}
+vim.lsp.enable({
+	'lua_ls',
+	'zls',
+	'rust-analyzer',
+})
+
+-----------------
+--- treesitter---
+-----------------
+
+vim.treesitter.language.add(
+	'javascript',
+	{ path = "/home/gabriel/.config/nvim/parser/js.so" }
+)
+vim.treesitter.language.add(
+	'rust',
+	{ path = "/home/gabriel/.config/nvim/parser/rust.so" }
+)
+vim.treesitter.language.add(
+	'zig',
+	{ path = "/home/gabriel/.config/nvim/parser/zig.so" }
+)
+vim.treesitter.language.add(
+	'html',
+	{ path = "/home/gabriel/.config/nvim/parser/html.so" }
+)
+vim.treesitter.language.add(
+	'css',
+	{ path = "/home/gabriel/.config/nvim/parser/css.so" }
+)
+vim.treesitter.language.add(
+	'tsx',
+	{ path = "/home/gabriel/.config/nvim/parser/tsx.so" }
+)
+vim.treesitter.language.add(
+	'typescript',
+	{ path = "/home/gabriel/.config/nvim/parser/ts.so" }
+)
+vim.treesitter.language.register('rust', { 'rust' })
+vim.treesitter.language.register('javascript', { 'js', 'jsx' })
+vim.treesitter.language.register('zig', { 'zig' })
+vim.treesitter.language.register('html', { 'html' })
+vim.treesitter.language.register('css', { 'css' })
+vim.treesitter.language.register('tsx', { 'typescriptreact' })
+vim.treesitter.language.register('typescript', { 'typescript' })
+
+vim.api.nvim_create_autocmd('FileType', {
+	pattern = { 'python', 'javascript', 'typescript', 'typescriptreact', 'rust', 'c', 'zig', 'html', 'css' },
+	callback = function()
+		vim.treesitter.start()
+	end,
+})
---
+++ .config/nvim/queries/js/locals.scm
@@ -0,0 +1,287 @@
+(((comment) @_jsdoc_comment
+  (#lua-match? @_jsdoc_comment "^/[*][*][^*].*[*]/$")) @injection.content
+  (#set! injection.language "jsdoc"))
+
+((comment) @injection.content
+  (#set! injection.language "comment"))
+
+; html(`...`), html`...`, sql(`...`), etc.
+(call_expression
+  function: (identifier) @injection.language
+  arguments: [
+    (arguments
+      (template_string) @injection.content)
+    (template_string) @injection.content
+  ]
+  (#lua-match? @injection.language "^[a-zA-Z][a-zA-Z0-9]*$")
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.include-children)
+  ; Languages excluded from auto-injection due to special rules
+  ; - svg uses the html parser
+  ; - css uses the styled parser
+  (#not-any-of? @injection.language "svg" "css"))
+
+; svg`...` or svg(`...`)
+(call_expression
+  function: (identifier) @_name
+  (#eq? @_name "svg")
+  arguments: [
+    (arguments
+      (template_string) @injection.content)
+    (template_string) @injection.content
+  ]
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.include-children)
+  (#set! injection.language "html"))
+
+; Vercel PostgreSQL
+; foo.sql`...` or foo.sql(`...`)
+(call_expression
+  function: (member_expression
+    property: (property_identifier) @injection.language)
+  arguments: [
+    (arguments
+      (template_string) @injection.content)
+    (template_string) @injection.content
+  ]
+  (#eq? @injection.language "sql")
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.include-children))
+
+; Sanity CMS GROQ query
+; defineQuery(`...`)
+(call_expression
+  function: (identifier) @_name
+  (#eq? @_name "defineQuery")
+  arguments: (arguments
+    (template_string) @injection.content)
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.include-children)
+  (#set! injection.language "groq"))
+
+; gql`...` or gql(`...`)
+(call_expression
+  function: (identifier) @_name
+  (#eq? @_name "gql")
+  arguments: [
+    (arguments
+      (template_string) @injection.content)
+    (template_string) @injection.content
+  ]
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.include-children)
+  (#set! injection.language "graphql"))
+
+(call_expression
+  function: (identifier) @_name
+  (#eq? @_name "hbs")
+  arguments: (template_string) @injection.content
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.include-children)
+  (#set! injection.language "glimmer"))
+
+; css`<css>`, keyframes`<css>`
+(call_expression
+  function: (identifier) @_name
+  (#any-of? @_name "css" "keyframes")
+  arguments: (template_string) @injection.content
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.include-children)
+  (#set! injection.language "styled"))
+
+; styled.div`<css>`
+(call_expression
+  function: (member_expression
+    object: (identifier) @_name
+    (#eq? @_name "styled"))
+  arguments: ((template_string) @injection.content
+    (#offset! @injection.content 0 1 0 -1)
+    (#set! injection.include-children)
+    (#set! injection.language "styled")))
+
+; styled(Component)`<css>`
+(call_expression
+  function: (call_expression
+    function: (identifier) @_name
+    (#eq? @_name "styled"))
+  arguments: ((template_string) @injection.content
+    (#offset! @injection.content 0 1 0 -1)
+    (#set! injection.include-children)
+    (#set! injection.language "styled")))
+
+; styled.div.attrs({ prop: "foo" })`<css>`
+(call_expression
+  function: (call_expression
+    function: (member_expression
+      object: (member_expression
+        object: (identifier) @_name
+        (#eq? @_name "styled"))))
+  arguments: ((template_string) @injection.content
+    (#offset! @injection.content 0 1 0 -1)
+    (#set! injection.include-children)
+    (#set! injection.language "styled")))
+
+; styled(Component).attrs({ prop: "foo" })`<css>`
+(call_expression
+  function: (call_expression
+    function: (member_expression
+      object: (call_expression
+        function: (identifier) @_name
+        (#eq? @_name "styled"))))
+  arguments: ((template_string) @injection.content
+    (#offset! @injection.content 0 1 0 -1)
+    (#set! injection.include-children)
+    (#set! injection.language "styled")))
+
+((regex_pattern) @injection.content
+  (#set! injection.language "regex"))
+
+; ((comment) @_gql_comment
+;   (#eq? @_gql_comment "/* GraphQL */")
+;   (template_string) @injection.content
+;   (#set! injection.language "graphql"))
+((template_string) @injection.content
+  (#lua-match? @injection.content "^`#graphql")
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.include-children)
+  (#set! injection.language "graphql"))
+
+; el.innerHTML = `<html>`
+(assignment_expression
+  left: (member_expression
+    property: (property_identifier) @_prop
+    (#any-of? @_prop "outerHTML" "innerHTML"))
+  right: (template_string) @injection.content
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.include-children)
+  (#set! injection.language "html"))
+
+; el.innerHTML = '<html>'
+(assignment_expression
+  left: (member_expression
+    property: (property_identifier) @_prop
+    (#any-of? @_prop "outerHTML" "innerHTML"))
+  right: (string
+    (string_fragment) @injection.content)
+  (#set! injection.language "html"))
+
+;---- Angular injections -----
+; @Component({
+;   template: `<html>`
+; })
+(decorator
+  (call_expression
+    function: ((identifier) @_name
+      (#eq? @_name "Component"))
+    arguments: (arguments
+      (object
+        (pair
+          key: ((property_identifier) @_prop
+            (#eq? @_prop "template"))
+          value: ((template_string) @injection.content
+            (#offset! @injection.content 0 1 0 -1)
+            (#set! injection.include-children)
+            (#set! injection.language "angular")))))))
+
+; @Component({
+;   styles: [`<css>`]
+; })
+(decorator
+  (call_expression
+    function: ((identifier) @_name
+      (#eq? @_name "Component"))
+    arguments: (arguments
+      (object
+        (pair
+          key: ((property_identifier) @_prop
+            (#eq? @_prop "styles"))
+          value: (array
+            ((template_string) @injection.content
+              (#offset! @injection.content 0 1 0 -1)
+              (#set! injection.include-children)
+              (#set! injection.language "css"))))))))
+
+; @Component({
+;   styles: `<css>`
+; })
+(decorator
+  (call_expression
+    function: ((identifier) @_name
+      (#eq? @_name "Component"))
+    arguments: (arguments
+      (object
+        (pair
+          key: ((property_identifier) @_prop
+            (#eq? @_prop "styles"))
+          value: ((template_string) @injection.content
+            (#set! injection.include-children)
+            (#offset! @injection.content 0 1 0 -1)
+            (#set! injection.language "css")))))))
+
+; Both properties are matched here.
+;
+;   class Foo {
+;     this.#bar = "baz";
+;     this.quuz = "qux";
+;   }
+(field_definition
+  property: [
+    (property_identifier)
+    (private_property_identifier)
+  ] @local.definition.var)
+
+; this.foo = "bar"
+(assignment_expression
+  left: (member_expression
+    object: (this)
+    property: (property_identifier) @local.definition.var))
+
+(formal_parameters
+  (identifier) @local.definition.parameter)
+
+; function(arg = []) {
+(formal_parameters
+  (assignment_pattern
+    left: (identifier) @local.definition.parameter))
+
+; x => x
+(arrow_function
+  parameter: (identifier) @local.definition.parameter)
+
+; ({ a }) => null
+(formal_parameters
+  (object_pattern
+    (shorthand_property_identifier_pattern) @local.definition.parameter))
+
+; ({ a: b }) => null
+(formal_parameters
+  (object_pattern
+    (pair_pattern
+      value: (identifier) @local.definition.parameter)))
+
+; ([ a ]) => null
+(formal_parameters
+  (array_pattern
+    (identifier) @local.definition.parameter))
+
+(formal_parameters
+  (rest_pattern
+    (identifier) @local.definition.parameter))
+
+; Both methods are matched here.
+;
+;   class Foo {
+;     #bar(x) { x }
+;     baz(y) { y }
+;   }
+(method_definition
+  [
+    (property_identifier)
+    (private_property_identifier)
+  ] @local.definition.function
+  (#set! definition.var.scope parent))
+
+; this.foo()
+(member_expression
+  object: (this)
+  property: (property_identifier) @local.reference)
