c0a7be2740a82a00d2436fde3bf8d8cf27db1e42
gabrieldemian <gabrielgcr45@gmail.com>
2026-05-04 22:16:54 +0200

    added rofi and eww

--- scripts/gencolors.sh
+++ scripts/gencolors.sh
@@ -32,7 +32,7 @@ for p in "${colors[@]}"; do
 	((index++))
 done
 
-echo -e "$alacritty" >"$HOME/.config/alacritty/colors.toml"
+echo -e "$alacritty" >"$HOME/dotfiles/.config/alacritty/colors.toml"
 
 # ====
 # NVIM
@@ -54,5 +54,44 @@ done
 nvim+="}\n"
 nvim+="return colors"
 
-# echo -e "$nvim"
-echo -e "$nvim" >"$HOME/.config/nvim/lua/colors.lua"
+echo -e "$nvim" >"$HOME/dotfiles/.config/nvim/lua/colors.lua"
+
+# ====
+# ROFI
+# ====
+
+rofi="* {\n"
+i=0
+
+for p in "${colors[@]}"; do
+	name=${colors_names[(($i % 8))]}
+	c="color${i}"
+	if [ "$i" -gt 7 ]; then
+		name+="bright"
+	fi
+	rofi+="\t$name: $(color $c);\n"
+	((i++))
+done
+
+rofi+="}"
+
+echo -e "$rofi" >"$HOME/dotfiles/.config/rofi/colors.rasi"
+
+# ===
+# EWW
+# ===
+
+i=0
+eww=""
+for p in "${colors[@]}"; do
+	name=${colors_names[(($i % 8))]}
+	c="color${i}"
+	if [ "$i" -gt 7 ]; then
+		name+="bright"
+	fi
+	eww+="\$$name: $(color $c);\n"
+	((i++))
+done
+
+echo -e "$eww" >"$HOME/dotfiles/.config/eww/colors.scss"
+stow -d "$HOME/dotfiles" -t "$HOME" .
