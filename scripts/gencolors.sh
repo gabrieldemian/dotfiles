3eb35f6c67d0d49b73060756fc28a4c2b507fc85
gabrieldemian <gabrielgcr45@gmail.com>
2026-05-10 23:00:21 +0200

    optimized gencolors.sh

--- scripts/gencolors.sh
+++ scripts/gencolors.sh
@@ -1,97 +1,106 @@
 #!/bin/bash
 
-color() {
-	xrdb -query | grep -E "$1" | sed -r "s/^[^:]+:\s+//" | head -n 1
-}
+set -e
+
+echo "generating color files..."
+
+color_names=(black red green yellow blue magenta cyan white)
 
 # generate color files for all programs that I care
 
-xrdb -load "$HOME/.colors"
-mapfile -t colors <"$HOME/.colors"
-index=0
+xrdb -load "$HOME/dotfiles/.colors"
+
+declare -A color_hex
+
+while IFS= read -r line; do
+	if [[ $line =~ (^|\*)color([0-9]+):[[:space:]]*(#[A-Fa-f0-9]+) ]]; then
+		index=${BASH_REMATCH[2]}
+		hex=${BASH_REMATCH[3]}
+		color_hex[$index]=$hex
+	fi
+done <"$HOME/dotfiles/.colors"
 
 # =========
 # ALACRITTY
 # =========
-colors_names=(black red green yellow blue magenta cyan white)
-alacritty=$(
-	cat <<-EOM
-		[colors]
-		[colors.primary]
-		background = "$(color color0)"
-		foreground = "$(color color15)"
-		[colors.normal]\n
-	EOM
-)
 
-for p in "${colors[@]}"; do
-	[[ "$index" -eq 8 ]] && alacritty+="[colors.bright]\n"
-	ala_name=${colors_names[(($index % 8))]}
-	c="color${index}"
-	alacritty+="$ala_name = \"$(color $c)\"\n"
-	((index++))
+alacritty="[colors]"$'\n'
+alacritty+="[colors.primary]"$'\n'
+alacritty+="background = \"${color_hex[0]}\""$'\n'
+alacritty+="foreground = \"${color_hex[15]}\""$'\n'
+alacritty+="[colors.normal]"$'\n'
+
+for ((i = 0; i < 16; i++)); do
+	((i == 8)) && alacritty+="[colors.bright]"$'\n'
+	name=${color_names[$i % 8]}
+	alacritty+="$name = \"${color_hex[$i]}\""$'\n'
 done
 
-echo -e "$alacritty" >"$HOME/dotfiles/.config/alacritty/colors.toml"
+echo -e "$alacritty"
+printf '%s' "$alacritty" >|"$HOME/dotfiles/.config/alacritty/colors.toml"
+
+# =======
+# GHOSTTY
+# =======
+
+ghostty+="background = ${color_hex[0]}"$'\n'
+ghostty+="foreground = ${color_hex[15]}"$'\n'
+ghostty+="cursor-color = ${color_hex[15]}"$'\n'
+ghostty+="cursor-text = ${color_hex[15]}"$'\n'
+ghostty+="selection-foreground = ${color_hex[0]}"$'\n'
+ghostty+="selection-background = ${color_hex[15]}"$'\n'
+
+for ((i = 0; i < 16; i++)); do
+	((i > 7)) && name+="_bright"
+	ghostty+="palette = $i=${color_hex[$i]}"$'\n'
+done
+
+printf '%s' "$ghostty" >|"$HOME/dotfiles/.config/ghostty/themes/main"
 
 # ====
 # NVIM
 # ====
 
-nvim="local colors = {\n"
-i=0
+nvim="local colors = {"$'\n'
 
-for p in "${colors[@]}"; do
-	name=${colors_names[(($i % 8))]}
-	c="color${i}"
-	if [ "$i" -gt 7 ]; then
-		name+="_bright"
-	fi
-	nvim+="\t$name = \"$(color $c)\",\n"
-	((i++))
+for ((i = 0; i < 16; i++)); do
+	name=${color_names[$i % 8]}
+	((i > 7)) && name+="_bright"
+	nvim+=$'\t'"$name = \"${color_hex[$i]}\","$'\n'
 done
 
-nvim+="}\n"
+nvim+="}"$'\n'
 nvim+="return colors"
 
-echo -e "$nvim" >"$HOME/dotfiles/.config/nvim/lua/colors.lua"
+printf '%s' "$nvim" >|"$HOME/dotfiles/.config/nvim/lua/colors.lua"
 
 # ====
 # ROFI
 # ====
 
-rofi="* {\n"
-i=0
+rofi="* {"$'\n'
 
-for p in "${colors[@]}"; do
-	name=${colors_names[(($i % 8))]}
-	c="color${i}"
-	if [ "$i" -gt 7 ]; then
-		name+="bright"
-	fi
-	rofi+="\t$name: $(color $c);\n"
-	((i++))
+for ((i = 0; i < 16; i++)); do
+	name=${color_names[$i % 8]}
+	((i > 7)) && name+="bright"
+	rofi+=$'\t'"$name: ${color_hex[$i]};"$'\n'
 done
 
 rofi+="}"
 
-echo -e "$rofi" >"$HOME/dotfiles/.config/rofi/colors.rasi"
+printf '%s' "$rofi" >|"$HOME/dotfiles/.config/rofi/colors.rasi"
 
 # ===
 # EWW
 # ===
 
-i=0
 eww=""
-for p in "${colors[@]}"; do
-	name=${colors_names[(($i % 8))]}
-	c="color${i}"
-	if [ "$i" -gt 7 ]; then
-		name+="bright"
-	fi
-	eww+="\$$name: $(color $c);\n"
-	((i++))
+for ((i = 0; i < 16; i++)); do
+	name=${color_names[$i % 8]}
+	((i > 7)) && name+="bright"
+	eww+="\$$name: ${color_hex[$i]};"$'\n'
 done
 
-echo -e "$eww" >"$HOME/dotfiles/.config/eww/colors.scss"
-# stow -d "$HOME/dotfiles" -t "$HOME" .
+printf '%s' "$eww" >|"$HOME/dotfiles/.config/eww/colors.scss"
+
+echo "done"
